26f557f27a2383ade1e8e44ad8bba5dc392522fa
freemontclubebr <freemontclubebrasil@gmail.com>
2025-01-30 14:37:18 -0300

    Update freemontbot.py

--- freemontbot.py
+++ freemontbot.py
@@ -12,19 +12,19 @@ def bot():
     # Dicionário de perguntas e respostas
     respostas = {
         "quantos litros de óleo de motor a freemont usa": "A Freemont usa 4,75 litros de óleo de motor.",
-        "qual óleo de motor especificado": "O óleo recomendado é 5W30 API/SN.",
-        "quantos litros de óleo de câmbio a freemont usa": "Troca completa: 9 Litros (Flush), troca parcial por gravidade: 6,5 Litros.",
-        "qual óleo de câmbio especificado": "Use sempre ATF+4, NÃO utilizar outra especificação.",
-        "quantos litros de aditivo de arrefecimento a freemont usa": "A Freemont usa 11 litros de aditivo de arrefecimento.",
-        "qual fluido de freio recomendado": "O fluido de freio recomendado é DOT 4, mas troque no circuito completo.",
-        "quais são os limites máximos de temperatura": (
-            "Limites máximos de temperatura:\n"
+        "Qual óleo de motor ?": "O óleo recomendado é 5W30 API/SN.",
+        "Quantos litros de óleo de câmbio a freemont usa ?": "Troca completa: 9 Litros (Flush), troca parcial por gravidade: 6,5 Litros.",
+        "Qual o óleo de câmbio ?": "Use sempre ATF+4, NÃO utilizar outra especificação.",
+        "Quantos litros de aditivo de arrefecimento a freemont usa ?": "A Freemont usa 11 litros de aditivo de arrefecimento.",
+        "Qual fluido de freio recomendado ?": "O fluido de freio recomendado é DOT 4, mas troque no circuito completo.",
+        "Quais são os limites de temperatura ?": (
+            "Limites de temperatura:\n"
             "Radiador: 110°C\n"
             "Óleo Motor: 120°C\n"
             "Óleo Câmbio: 120°C"
         ),
-        "qual bateria utilizar": "Bateria 60 Ah, polo positivo invertido ou esquerdo.",
-        "manutenção preventiva": (
+        "Qual bateria utilizar ?": "Bateria 60 Ah, polo positivo invertido ou esquerdo.",
+        "Manutenção preventiva": (
             "🔧 **Manutenção preventiva:**\n"
             "- Troque os 2 Ts do ar quente e o T do radiador se necessário (após 50.000 km).\n"
             "- Se o reservatório de expansão borbulhar, troque a tampa metálica do radiador.\n"
@@ -45,7 +45,7 @@ def bot():
     if resposta_encontrada:
         msg.body(resposta_encontrada)
     else:
-        msg.body("Desculpe, não entendi. Pergunte algo como: 'óleo de motor', 'óleo de câmbio', 'manutenção preventiva'.")
+        msg.body("Desculpe, não entendi. Pergunte algo como: 'Óleo de motor', 'Óleo de câmbio', 'Temperaturas','Manutenção preventiva', 'Bateria'.")
 
     return str(response)
 
